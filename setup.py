a761ca52d361925f7c0e03c4248a23c5c8714a27
Vincent Fretin <vincent.fretin@gmail.com>
2013-02-07 12:37:21 +0100

    initial structure

---
+++ setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""Installer for the collective.contact package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+
+setup(
+    name='collective.contact',
+    version='1.0',
+    description="Contact widget",
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 4.2",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='',
+    author='Vincent Fretin',
+    author_email='vincentfretin@ecreall.com',
+    url='http://pypi.python.org/pypi/collective.contact',
+    license='GPL',
+    packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['collective', 'collective.contact'],
+    package_dir={'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'five.grok',
+        'setuptools',
+    ],
+    extras_require={
+        'test': [
+            'ecreall.helpers.testing',
+            'plone.app.testing',
+        ],
+    },
+    entry_points="""
+    """,
+)
